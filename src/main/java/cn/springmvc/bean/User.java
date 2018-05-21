94ee8486bf8a257da2497e8687d4218b14e400a0
GuoShengJie <guoshengjie_sv@163.com>
2018-05-21 20:41:06 +0800

    first commit

---
+++ src/main/java/cn/springmvc/bean/User.java
@@ -0,0 +1,65 @@
+package cn.springmvc.bean;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public User(String id, String name, String address, String email) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.address = address;
+		this.email = email;
+	}
+
+	public User() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	private String name;
+	private String address;
+	private String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", address=" + address
+				+ ", email=" + email + "]";
+	}
+
+}
---
+++ src/main/java/cn/springmvc/controller/UserTest.java
@@ -0,0 +1,16 @@
+package cn.springmvc.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import cn.springmvc.biz.UserBiz;
+
+public class UserTest {
+
+	
+	public static void main(String[] args) {
+		ApplicationContext ap=new ClassPathXmlApplicationContext("spring.xml");
+		UserBiz userBiz=(UserBiz) ap.getBean("userBiz");
+		userBiz.getData();
+	}
+}
